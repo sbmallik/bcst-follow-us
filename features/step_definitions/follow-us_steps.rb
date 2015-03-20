a8be9092e6f9ce42bd060f9205cd70faa4f2c9b6
Mallik <smallik@gannett.com>
2015-03-20 10:47:32 -0400

    Minor update to create a couple of procedures.

--- features/step_definitions/follow-us_steps.rb
+++ features/step_definitions/follow-us_steps.rb
@@ -1,3 +1,18 @@
+def new_window_handler
+	@original_window = page.driver.browser.window_handles.first
+	@new_window = page.driver.browser.window_handles.last
+	page.driver.browser.switch_to.window @new_window
+	yield
+	page.driver.browser.close
+	page.driver.browser.switch_to.window @original_window
+end
+
+def click_page_link(class_name)
+	@page_element = find(class_name)
+	$expected_url = @page_element[:href].sub!(/^.*\//,'')
+	@page_element.click
+end
+
 Given(/^I am on the Home front in Broadcast site$/) do
 	visit ui_url('/')
 	wait_for_pageload
@@ -12,16 +27,11 @@ Given(/^The "(.*?)" button exists$/) do | media_name |
 end
 
 When(/^I click the "(.*?)" button$/) do | media_name |
-	@page_element = find(".social-media-#{media_name.downcase.tr(' ','_')}")
-	$expected_url = @page_element[:href].sub!(/^.*\//,'')
-	@page_element.click
+	click_page_link(".social-media-#{media_name.downcase.tr(' ','_')}")
 end
 
 Then(/^The target site loads in a new window$/) do
-	@original_window = page.driver.browser.window_handles.first
-	@new_window = page.driver.browser.window_handles.last
-	page.driver.browser.switch_to.window @new_window
-	expect(current_url.sub!(/^.*\//,'')).to match $expected_url
-	page.driver.browser.close
-	page.driver.browser.switch_to.window @original_window
+	new_window_handler do
+		expect(current_url.sub!(/^.*\//,'')).to match $expected_url
+	end
 end
